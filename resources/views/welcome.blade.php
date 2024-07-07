62b499cdecf657d4778c6976a8ad2023eb3dc743
Dazannah <beattius@gmail.com>
2024-07-07 13:31:19 +0200

    dashboard base layout and new style added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+<x-guest-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             Welcome to the appointment site
@@ -14,4 +14,4 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+</x-guest-layout>
