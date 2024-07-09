531af4239188795987383629d226224ff50c6079
Dazannah <beattius@gmail.com>
2024-07-09 20:02:41 +0200

    dark mode button added to gest layout

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,37 @@
+<script>
+  if(!localStorage.getItem('dark-mode')){
+    localStorage.setItem('dark-mode', window.matchMedia('(prefers-color-scheme: dark)').matches)
+  }
+
+  if (localStorage.getItem('dark-mode') === 'true' || (!('dark-mode' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.querySelector('html').classList.add('dark');
+  } else {
+    document.querySelector('html').classList.remove('dark');
+  }
+
+  const lightSwitches = document.querySelectorAll('.light-switch');
+  
+  if (lightSwitches.length > 0) {
+    lightSwitches.forEach((lightSwitch, i) => {
+      if (localStorage.getItem('dark-mode') === 'true') {
+        lightSwitch.checked = true;
+      }
+      lightSwitch.addEventListener('change', () => {
+        const { checked } = lightSwitch;
+        lightSwitches.forEach((el, n) => {
+          if (n !== i) {
+            el.checked = checked;
+          }
+        });
+        if (lightSwitch.checked) {
+          document.documentElement.classList.add('dark');
+          localStorage.setItem('dark-mode', true);
+        } else {
+          document.documentElement.classList.remove('dark');
+          localStorage.setItem('dark-mode', false);
+        }
+      });
+    });
+  }
+</script>
+</html>
