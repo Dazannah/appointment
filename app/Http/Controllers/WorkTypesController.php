11eb38d341b7481ecd6057502e78c91b0c6531f6
Dazannah <beattius@gmail.com>
2024-08-05 19:26:56 +0200

    added work type controller

---
+++ app/Http/Controllers/WorkTypesController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\WorkTypes;
+use Illuminate\Http\Request;
+
+class WorkTypesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(WorkTypes $workTypes)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(WorkTypes $workTypes)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, WorkTypes $workTypes)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(WorkTypes $workTypes)
+    {
+        //
+    }
+}
