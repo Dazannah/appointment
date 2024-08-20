14ad7436dbe7d4254d8839dbb31a143d3d394a1c
Dazannah <beattius@gmail.com>
2024-08-20 16:45:23 +0200

    deleted this

--- resources/views/manage-event.blade.php
+++ resources/views/manage-event.blade.php
@@ -16,7 +16,7 @@
     </div>
     <div class="flex items-start mb-5">
       <div class="flex items-center h-5">
-        <input id="delete" onchange="confirmDelete(this)" name="delete" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" />
+        <input id="delete" onchange="confirmDelete()" name="delete" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" />
       </div>
       <label for="delete" class="ms-2 text-sm font-medium text-red-500 dark:text-red-600">Delete</label>
     </div>
