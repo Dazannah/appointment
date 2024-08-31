c5b82203e387b8bb6fd7c4f54a9ee75c3b9300af
Dazannah <beattius@gmail.com>
2024-08-31 16:19:04 +0200

    commented out menuitem

--- resources/views/components/admin-secondary-menu.blade.php
+++ resources/views/components/admin-secondary-menu.blade.php
@@ -6,8 +6,8 @@
         <a class="flex items-center flex-shrink-0 h-10 px-2 text-sm font-medium rounded hover:bg-gray-300 dark:hover:bg-gray-800 {{request()->routeIs('admin.menu.events') ? 'bg-gray-300 dark:bg-gray-800' : ''}}" href="/admin/menu/events">
             <span class="leading-none">Appointments</span>
         </a>
-        <a class="flex items-center flex-shrink-0 h-10 px-2 text-sm font-medium rounded hover:bg-gray-300 dark:hover:bg-gray-800" href="#">
+        {{--<a class="flex items-center flex-shrink-0 h-10 px-2 text-sm font-medium rounded hover:bg-gray-300 dark:hover:bg-gray-800" href="#">
             <span class="leading-none">item 3</span>
-        </a>
+        </a>--}}
     </div>
 </div>
