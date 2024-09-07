1bd4f149542335b9f67ccfe77b371fe2527b6b51
Dazannah <beattius@gmail.com>
2024-09-07 20:27:34 +0200

    updated ISiteConfig and implemented functions in SiteConfigService

--- app/Interfaces/ISiteConfig.php
+++ app/Interfaces/ISiteConfig.php
@@ -4,4 +4,8 @@ namespace App\Interfaces;
 
 interface ISiteConfig {
     public function getConfig();
+    public function setConfig(array $newConfigs): void;
+    public function save(): void;
+    public function updateConfigs(&$newConfigs, &$oldConfigs): void;
+    public function serialiseInputs($validatedInputs): array;
 }
--- app/Services/SiteConfigService.php
+++ app/Services/SiteConfigService.php
@@ -8,10 +8,41 @@ class SiteConfigService implements ISiteConfig {
   private $configs;
 
   public function __construct() {
-    $this->configs = require(base_path() . '/config/adminConfigs.php');
+    $jsonString = file_get_contents(base_path() . '/config/adminConfigs.json');
+    $this->configs = json_decode($jsonString, true);
   }
 
-  public function getConfig() {
+  public function getConfig(): array {
     return $this->configs;
+  }
+
+  public function setConfig(array $newConfigs): void {
+    $this->updateConfigs($newConfigs, $this->configs);
+  }
+
+  public function save(): void {
+    $jsonString = json_encode($this->configs);
+    file_put_contents(base_path() . '/config/adminConfigs.json', $jsonString);
+  }
+
+  public function updateConfigs(&$newConfigs, &$oldConfigs): void {
+    $allKeys = array_keys($newConfigs);
+
+    foreach ($allKeys as $key) {
+      if (gettype($oldConfigs["$key"]) == 'array') {
+        $this->updateConfigs($newConfigs["$key"], $oldConfigs["$key"]);
+      } else {
+        $oldConfigs["$key"] = $newConfigs["$key"];
+      }
+    }
+  }
+
+  public function serialiseInputs($validatedInputs): array {
+    $result = array();
+
+    if ($this->configs['calendarTimes']['slotMinTime'] !== $validatedInputs['workdayStart']) $result['calendarTimes']['slotMinTime'] = $validatedInputs['workdayStart'];
+    if ($this->configs['calendarTimes']['slotMaxTime'] !== $validatedInputs['workdayEnd']) $result['calendarTimes']['slotMaxTime'] = $validatedInputs['workdayEnd'];
+
+    return $result;
   }
 }
