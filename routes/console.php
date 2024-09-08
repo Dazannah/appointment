72287f1e00b184372a31661f6307b1eb8d9367d0
Dazannah <beattius@gmail.com>
2024-09-08 17:23:06 +0200

    close all events every fifteenMinutes

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\EventController;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('closeEvents', function () {
+    $eventController = app(EventController::class);
+    $eventController->closeEventInPastIfNotClosedOrDeleted();
+})->purpose('Start close all events')->everyFifteenMinutes();
