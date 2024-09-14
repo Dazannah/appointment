4b64a80b26b728525fecb9760c26821e939f26d5
Dazannah <beattius@gmail.com>
2024-09-14 18:14:22 +0200

    serialiseInputForCreateClosedDay added to interface and implemented

--- app/Interfaces/IDataSerialisation.php
+++ app/Interfaces/IDataSerialisation.php
@@ -5,4 +5,5 @@ namespace App\Interfaces;
 interface IDataSerialisation {
   public function serialiseInputForEditEvent($validated, $event): void;
   public function serialiseInputForEditUser($validated, $user): void;
+  public function serialiseInputForCreateClosedDay($validated): array;
 }
