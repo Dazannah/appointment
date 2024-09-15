3bc1efee6969b65a53c4ab6c80a5b5c883dfbe88
Dazannah <beattius@gmail.com>
2024-09-15 17:05:20 +0200

    added fillHolidays function

--- app/Http/Controllers/SiteConfigController.php
+++ app/Http/Controllers/SiteConfigController.php
@@ -2,15 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Interfaces\IClosedDay;
+use App\Interfaces\IRequest;
 use App\Interfaces\ISiteConfig;
 use Illuminate\Http\Request;
 
 class SiteConfigController extends Controller {
 
     private ISiteConfig $siteConfigs;
+    private IClosedDay $closedDayService;
 
-    public function __construct(ISiteConfig $siteConfigs) {
+    public function __construct(ISiteConfig $siteConfigs, IClosedDay $closedDayService) {
         $this->siteConfigs = $siteConfigs;
+        $this->closedDayService = $closedDayService;
     }
 
     public function fillHolidays(Request $req) {
@@ -19,11 +23,16 @@ class SiteConfigController extends Controller {
         ]);
 
         //make request
+        $requestService = app(IRequest::class);
+        $holidays = $requestService->getHolidays($validated['year']);
 
-        //go through all date and check if it is already saved if not save it, ignore the work days(type: 2)
-
-        echo $validated['year'];
-        exit;
+        if ($holidays['response'] === 'Error') {
+            return back()->withInput()->with('error', $holidays['message']);
+        } else {
+            $this->closedDayService->handleHolidays($holidays['days'], $validated['year']);
+            //go through all date and check if it is already saved if not save it, ignore the work days(type: 2)
+            return back()->with('success', 'Holidays successfully updated');
+        }
     }
 
     public function index() {
