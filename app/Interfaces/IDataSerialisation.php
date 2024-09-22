c484447a1371212341d6fb9d73ec4347bac91272
Dazannah <39139379+Dazannah@users.noreply.github.com>
2024-09-22 16:02:06 +0200

    Tests (#61)
    * commented out feature directory
    
    * creted short cut to run test
    
    * deleted example test
    
    * created ClosedDayMock to be able modify create method
    
    * made dependency injection ClosedDay and added IDataSerialisation, plus changed ClosedDay static calls
    
    * added serialseClosedDaysForCalendar
    
    * added 'is_admin' => false to definition
    
    * created ClosedDayServiceTest
    
    * getUserId() function created for testing purpose and serialseClosedDaysForCalendar modifyed
    
    * typos corrected
    
    * print_rs removed
    
    * DataSerialisationServiceTest created

--- app/Interfaces/IDataSerialisation.php
+++ app/Interfaces/IDataSerialisation.php
@@ -2,8 +2,12 @@
 
 namespace App\Interfaces;
 
+use Illuminate\Database\Eloquent\Collection;
+
 interface IDataSerialisation {
   public function serialiseInputForEditEvent($validated, $event): void;
   public function serialiseInputForEditUser($validated, $user): void;
   public function serialiseInputForCreateClosedDay($validated): array;
+  public function serialseClosedDaysForCalendar($closedDays): Collection;
+  public function getUserId(): int;
 }
