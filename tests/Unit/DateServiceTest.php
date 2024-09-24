ecb1709f68ed26e482435745c93540fd5649b90b
Dazannah <beattius@gmail.com>
2024-09-24 21:13:41 +0200

    finished DateServiceTest

--- tests/Unit/DateServiceTest.php
+++ tests/Unit/DateServiceTest.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\Event;
 use App\Services\DateService;
 use App\Services\SiteConfigService;
+use Illuminate\Database\Eloquent\Collection;
 use PHPUnit\Framework\TestCase;
 use PHPUnit\Framework\Attributes\Test;
 
@@ -157,5 +159,203 @@ class DateServiceTest extends TestCase {
         "Test case {$index} failed."
       );
     }
+  }
+
+  #[Test]
+  public function IsEndAfterClose() {
+    $testCases = [
+      [
+        'end' => '2024-09-24T16:00',
+        'result' => false
+      ],
+      [
+        'end' => '2024-09-24T16:01',
+        'result' => true
+      ],
+      [
+        'end' => '2024-09-24T15:59',
+        'result' => false
+      ],
+    ];
+
+    foreach ($testCases as $index => $case) {
+      $this->assertEquals(
+        $case['result'],
+        $this->dateService->IsEndAfterClose($case['end']),
+        "Test case {$index} failed."
+      );
+    }
+  }
+
+  #[Test]
+  public function IsStartBeforeOpen() {
+    $testCases = [
+      [
+        'start' => '2024-09-24T08:00',
+        'result' => false
+      ],
+      [
+        'start' => '2024-09-24T07:59',
+        'result' => true
+      ],
+      [
+        'start' => '2024-09-24T08:01',
+        'result' => false
+      ],
+    ];
+
+    foreach ($testCases as $index => $case) {
+      $this->assertEquals(
+        $case['result'],
+        $this->dateService->IsStartBeforeOpen($case['start']),
+        "Test case {$index} failed."
+      );
+    }
+  }
+
+  #[Test]
+  public function GetMinutsFromDateDiff() {
+    $testCases = [
+      [
+        'dateInteval' => DateInterval::createFromDateString('1 day'),
+        'result' => 1440
+      ],
+      [
+        'dateInteval' => DateInterval::createFromDateString('0 day'),
+        'result' => 0
+      ],
+      [
+        'dateInteval' => DateInterval::createFromDateString('3 hour'),
+        'result' => 180
+      ],
+    ];
+
+    foreach ($testCases as $index => $case) {
+      $this->assertEquals(
+        $case['result'],
+        $this->dateService->GetMinutsFromDateDiff($case['dateInteval']),
+        "Test case {$index} failed."
+      );
+    }
+  }
+
+  #[Test]
+  public function IsStartAndEndDifferenceEqualWithEventDuration() {
+    $testCases = [
+      [
+        'start' => '2024-09-24T08:00',
+        'end' => '2024-09-24T08:30',
+        'duration' => '30',
+        'result' => true
+      ],
+      [
+        'start' => '2024-09-24T09:00',
+        'end' => '2024-09-24T08:30',
+        'duration' => '30',
+        'result' => false
+      ],
+      [
+        'start' => '2024-09-24T08:00',
+        'end' => '2024-09-24T08:30',
+        'duration' => '31',
+        'result' => false
+      ],
+    ];
+
+    foreach ($testCases as $index => $case) {
+      $this->assertEquals(
+        $case['result'],
+        $this->dateService->IsStartAndEndDifferenceEqualWithEventDuration($case['start'], $case['end'], $case['duration']),
+        "Test case {$index} failed."
+      );
+    }
+  }
+
+  #[Test]
+  public function getNextEventDate() {
+    $testCases = [
+      [
+        'startDate' => '2024-09-24T08:00',
+        'event' => ['start' => '2024-09-24T12:00'],
+        'result' => '2024-09-24T12:00'
+      ],
+      [
+        'startDate' => '2024-09-24T08:00',
+        'event' => null,
+        'result' => '2024-09-24T16:00'
+      ],
+      [
+        'startDate' => '2024-09-24T08:00',
+        'event' => ['start' => '2024-09-25T12:00'],
+        'result' => '2024-09-24T16:00'
+      ],
+      [
+        'startDate' => '2024-09-24T08:00',
+        'event' => ['start' => '2024-09-23T12:00'],
+        'result' => '2024-09-24T16:00'
+      ],
+    ];
+
+    foreach ($testCases as $index => $case) {
+      $this->assertEquals(
+        $case['result'],
+        $this->dateService->getNextEventDate($case['startDate'], $case['event']),
+        "Test case {$index} failed."
+      );
+    }
+  }
+
+  #[Test]
+  public function replaceTInStartEnd() {
+
+    $appointments = new Collection(
+      [
+        new Event([
+          'start' => '2024-09-24T10:00',
+          'end' => '2024-09-24T10:30',
+          'title' => 'This is the title',
+          'user_id' => 1,
+          'work_type_id' => 1,
+          'note' => '',
+          'status_id' => 1
+        ]),
+        new Event([
+          'start' => '2024-09-24 10:00',
+          'end' => '2024-09-24 10:30',
+          'title' => 'This is the title',
+          'user_id' => 1,
+          'work_type_id' => 1,
+          'note' => '',
+          'status_id' => 1
+        ])
+      ]
+    );
+
+    $result = new Collection(
+      [
+        new Event([
+          'start' => '2024-09-24 10:00',
+          'end' => '2024-09-24 10:30',
+          'title' => 'This is the title',
+          'user_id' => 1,
+          'work_type_id' => 1,
+          'note' => '',
+          'status_id' => 1
+        ]),
+        new Event([
+          'start' => '2024-09-24 10:00',
+          'end' => '2024-09-24 10:30',
+          'title' => 'This is the title',
+          'user_id' => 1,
+          'work_type_id' => 1,
+          'note' => '',
+          'status_id' => 1
+        ])
+      ]
+    );
+
+    $this->dateService->replaceTInStartEnd($appointments);
+
+    $this->assertEquals($result, $appointments);
   }
 }
