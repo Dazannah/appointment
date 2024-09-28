7c2adfe7aa419a494f8d41597aa390eeff9b73fa
Dazannah <beattius@gmail.com>
2024-09-28 20:53:24 +0200

    rolled back to use default Request becouse i found $request->wantsJson()

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\CustomRequest;
+use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
@@ -14,4 +14,4 @@ require __DIR__ . '/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
 (require_once __DIR__ . '/../bootstrap/app.php')
-    ->handleRequest(CustomRequest::capture());
+    ->handleRequest(Request::capture());
